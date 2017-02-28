1e5f37cab76ad6fced2c685ba8b8b98c013f8f74
Olivier Fambon <olivier.fambon@inria.fr>
2017-02-28 12:47:22 +0100

    setup: initial ci

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+PACKAGE = "embers.injectors"
+VERSION = "0.1"
+
+
+setup(
+    name           = PACKAGE,
+    version        = VERSION,
+    author         = "The EMBERS consortium",
+    author_email   = "dev@embers.city",
+    description    = "Injectors for EMBERS",
+    url            = "http://www.embers.city/",
+    keywords       = ["Open Data", "Smart City"],
+    license        = "=== TBD ===",
+    packages       = find_packages("src"),
+    package_dir    = {"": "src"},
+    namespace_packages = [PACKAGE],
+
+    install_requires = [
+        "embers.meshblu",
+        "embers.datasets",
+    ],
+)
