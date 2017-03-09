920ad658de20740be3659d201fe3a2225938da5f
Olivier Fambon <olivier.fambon@inria.fr>
2017-03-09 12:17:43 +0100

    tests: add test_injectors_init_config

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,6 +7,21 @@ def test_injectors_no_args():
     run("injectors")
 
 
+def test_injectors_init_config():
+    run("injectors --init-config")
+    from embers.injectors.config import get_config
+    config = get_config()
+    assert config.broker_address
+    assert config.root_auth
+
+
+def test_injectors_init_config_with_broker():
+    run("injectors --init-config --broker localhost")
+    from embers.injectors.config import get_config
+    config = get_config(force_reload=True)
+    assert config.broker_address == "localhost"
+
+
 def test_injectors_run_defaults():
     run("injectors --run")
 
