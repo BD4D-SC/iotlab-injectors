865ba3791ca5b0266c24f4730d37e329d5b2b4ba
Olivier Fambon <olivier.fambon@inria.fr>
2017-03-11 23:29:05 +0100

    tests: add test_unregister_parking

--- tests/test_zzz_cleanup.py
+++ tests/test_zzz_cleanup.py
@@ -10,6 +10,15 @@ def test_check_init_needed():
        run("registry --init")
 
 
+def test_unregister_parking():
+    ret = run("registry --list")
+    for line in ret.split('\n'):
+        if "parking" not in line:
+            continue
+        uuid = line.split()[0]
+        run("registry --unregister --uuid " + uuid)
+
+
 def test_unregister_superfluous_root_auth(config):
     ret = run("registry --list")
     for line in ret.split('\n'):
