a7e13883ce571f6ef0ef61eb655075963eef7b38
Olivier Fambon <olivier.fambon@inria.fr>
2017-03-11 22:22:40 +0100

    registry: add lookup_gateway/devices()

--- src/embers/injectors/registry.py
+++ src/embers/injectors/registry.py
@@ -12,16 +12,34 @@ def DeviceMetadata(dataset):
 
 def register_gateway(dataset):
     metadata = GatewayMetadata(dataset)
-    api = get_broker_api()
-    ret = api.get_devices(metadata)
-
-    if ret["devices"]:
+    gateway = _lookup_devices(metadata)
+    if gateway:
         return False
     else:
-        return api.register_device(metadata)
+        return _register_device(metadata)
 
 
 def register_device(dataset):
     metadata = DeviceMetadata(dataset)
+    return _register_device(metadata)
+
+
+def lookup_gateway(dataset):
+    metadata = GatewayMetadata(dataset)
+    ret = _lookup_devices(metadata)
+    return ret[0] if ret else None
+
+
+def lookup_devices(dataset):
+    metadata = DeviceMetadata(dataset)
+    return _lookup_devices(metadata)
+
+
+def _register_device(metadata):
     api = get_broker_api()
     return api.register_device(metadata)
+
+
+def _lookup_devices(metadata):
+    api = get_broker_api()
+    return api.get_devices(metadata)["devices"]
