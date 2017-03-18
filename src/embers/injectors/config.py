9e493b031efc6af1f883198edfc658b5f770998a
Olivier Fambon <olivier.fambon@inria.fr>
2017-03-18 12:42:31 +0100

    config: add load_config_exit_on_fail(), use it in get_config()

--- src/embers/injectors/config.py
+++ src/embers/injectors/config.py
@@ -8,7 +8,7 @@ class config: pass  # namespace
 
 def get_config(force_reload=False):
     if force_reload or not is_loaded():
-        load_config()
+        load_config_exit_on_fail()
     assert config.broker_address
     assert config.root_auth
     return config()
@@ -19,6 +19,15 @@ def get_broker_api(auth=None):
     api = http.Client(config.broker_address)
     api.auth = config.root_auth if auth is None else auth
     return api
+
+
+def load_config_exit_on_fail():
+    try:
+        load_config()
+    except Exception as e:
+        import sys
+        print("failed to load {}: {}".format(CONFIG_FILE, e))
+        sys.exit(1)
 
 
 def init_config(broker_address):
