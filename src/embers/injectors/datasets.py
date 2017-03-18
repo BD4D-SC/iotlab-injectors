80af7113780d2f7aca6f0236f25126aa1b1b80c1
Olivier Fambon <olivier.fambon@inria.fr>
2017-03-18 09:50:37 +0100

    datasets: add get_dataset()

--- src/embers/injectors/datasets.py
+++ src/embers/injectors/datasets.py
@@ -12,3 +12,10 @@ def get_datasets():
         mods += [modname]
         #module = __import__(modname) #, fromlist="dummy")
     return mods
+
+
+def get_dataset(dataset, event_type):
+    package_name = "embers.datasets." + dataset + "." + event_type
+    class_name = event_type.capitalize()
+    package = __import__(package_name, fromlist=[class_name])
+    return package.__dict__[class_name]()
