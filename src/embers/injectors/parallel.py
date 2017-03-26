27813fb097b8c16dc16945966bc1f28d036325ea
Olivier Fambon <olivier.fambon@inria.fr>
2017-03-26 17:40:00 +0200

    parallel: handle errors

--- src/embers/injectors/parallel.py
+++ src/embers/injectors/parallel.py
@@ -4,12 +4,16 @@ MAX_THREADS = 100
 def parallel_run(params, run, collect=lambda th:th):
     class runner(threading.Thread):
         def run(self):
-            run(self.param, self)
+            try:
+                run(self.param, self)
+            except Exception as e:
+                self.error = e
 
     t = []
     for param in params:
         th = runner()
         th.param = param
+        th.error = None
         th.start()
         t.append(th)
         if len(t) == MAX_THREADS:
@@ -18,7 +22,12 @@ def parallel_run(params, run, collect=lambda th:th):
 
 
 def join_all(t, func=lambda th:th):
+    error = None
     while len(t):
         th = t.pop()
         th.join()
+        error = error or th.error
+        if error: continue
         func(th)
+    if error:
+        raise error
