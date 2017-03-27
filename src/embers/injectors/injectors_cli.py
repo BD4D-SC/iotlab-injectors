c5cba52c38a16c5122cf4e86e09007f7dde7e31a
Olivier Fambon <olivier.fambon@inria.fr>
2017-03-27 08:56:28 +0200

    injector: isolate register_devices() in separate function

--- src/embers/injectors/injectors_cli.py
+++ src/embers/injectors/injectors_cli.py
@@ -33,12 +33,7 @@ def run(nb_devices, events, dataset, protocol, ev_per_hour, duration, **_):
     print("running {nb} '{}+{}' injector{s} on local node".format(
           events, protocol, nb=nb_devices, s=s(nb_devices)))
 
-    gateway = registry.lookup_gateway(events)
-    if not gateway:
-        gateway = registry.register_gateway(events)
-        print("==> registered gateway '{}'".format(events))
-
-    devices = registry.register_devices(events, nb_devices)
+    gateway, devices = register_devices(events, nb_devices)
 
     print("sending {} event{s}/h (per injector) for {} min.".format(
           ev_per_hour, duration, s=s(ev_per_hour)))
@@ -59,6 +54,17 @@ def run(nb_devices, events, dataset, protocol, ev_per_hour, duration, **_):
             registry.unregister_devices(devices)
         except:
             pass
+
+
+def register_devices(events, nb_devices):
+    gateway = registry.lookup_gateway(events)
+    if not gateway:
+        gateway = registry.register_gateway(events)
+        print("==> registered gateway '{}'".format(events))
+
+    devices = registry.register_devices(events, nb_devices)
+
+    return gateway, devices
 
 
 @command
