9873d2c6955587d81c571069a2935227d0dfbb3a
Olivier Fambon <olivier.fambon@inria.fr>
2017-03-27 19:26:03 +0200

    deploy: move 'generic' code to lib.sh

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,15 @@
 #!/bin/bash -e
-#set -x
+
+source lib.sh
+
 
 set_params() {
 	NB_NODES=${1:-2}
 	DURATION=${2:-5}
-
-	BROKER=${BROKER:-DEFINE_MESHBLU.broker.fqdn}
-
 	IOTLAB_SITE=${3:-grenoble}
+
+	BROKER=${BROKER:-meshblu.octoblu.com}
 }
-
-LOCAL_OVERRIDES="deploy.local.sh"
-
-LOG=${LOG:-$0.log}
-
 
 main() {
 	_ start_experiment
@@ -26,39 +22,6 @@ main() {
 
 	_ deploy_injectors
 	_ run_injectors
-}
-
-start_experiment() {
-	exp_id=$(experiment-cli submit -d $DURATION \
-		-l $NB_NODES,archi=a8:at86rf231+site=$IOTLAB_SITE \
-		| awk '/"id":/ {print $2}'
-	)
-	experiment-cli wait -i $exp_id &>/dev/null
-}
-
-get_nodes() {
-	experiment-cli get -i $exp_id -r \
-	| awk '/network_address/ {print $2}' \
-	| tr -d '",'
-}
-
-wait_for_boot() {
-	time_start=`date +%s`
-	while true; do
-		nb_booted=`get_booted_nodes | wc -l`
-		time_diff=$[`date +%s` - time_start]
-		[ $nb_booted = $NB_NODES ] && break
-		[ $time_diff -gt 80 ] && break
-		#echo "nb_booted=$nb_booted time_diff=$time_diff"
-		sleep 1
-	done
-}
-
-get_booted_nodes() {
-	for node in $nodes; do
-		(check_ssh_access && echo $node) &
-	done
-	wait
 }
 
 deploy_node() {
@@ -93,69 +56,6 @@ run_injectors() {
 	done
 }
 
-check_ssh_access() {
-	ssh $node -o ConnectTimeout=2 id &>/dev/null
-}
-
-animated_wait() {
-	while true; do
-		for c in . o O 0 O o; do
-			printf "\r%-30s   %c  " $func $c
-			sleep .3
-		done
-	done
-}
-
-_() {
-	func=$1
-	animated_wait & _pid=$!
-	disown $_pid
-	log "=== $func"
-	$func &>> $LOG
-	kill $_pid && _pid=
-	printf "\r%-30s [%s]\n" $func "done"
-}
-
-on_error() {
-	[ $_pid ] && kill $_pid
-	echo "ERROR"
-	echo
-	echo "last line in $LOG:"
-	tail -1 $LOG
-	exit 1
-}
-
-init_exit_trap() {
-	exec 3>&1
-	trap '[ $? = 0 ] || on_error >&3' EXIT
-}
-
-log() {
-	date +"%F %T $*" >> $LOG
-}
-
-check_nb_nodes() {
-	nb_nodes=`wc -w <<< $nodes`
-	[ $nb_nodes = $NB_NODES ] && return
-
-	msg="!!! deploying on $nb_nodes nodes"
-	log  "$msg"
-	echo "$msg"
-}
-
-init() {
-	init_exit_trap
-	log_trace set_params $@
-}
-
-log_trace() {
-	set -x
-	$* &>> $LOG
-	set +x
-} &>/dev/null
-
-
-[ -f $LOCAL_OVERRIDES ] && source $LOCAL_OVERRIDES
 
 log "=== starting ===="
 init "$@"
