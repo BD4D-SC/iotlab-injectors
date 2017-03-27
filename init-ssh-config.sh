648222aaa0fa3ebceb88adcdda424dc42ca37402
Olivier Fambon <olivier.fambon@inria.fr>
2017-03-27 18:32:31 +0200

    init-ssh-config: add LogLevel and ControlMaster options

--- init-ssh-config.sh
+++ init-ssh-config.sh
@@ -5,10 +5,17 @@ Host a8-*
 User root
 StrictHostKeyChecking no
 UserKnownHostsFile /dev/null
+LogLevel ERROR
 
 Host a8-*.grenoble.*
 ProxyCommand ssh grenoble.iot-lab.info -W node-%h:%p
 
 Host a8-*.saclay.*
 ProxyCommand ssh saclay.iot-lab.info -W node-%h:%p
+
+# prevent ssh-server connections rate-limit issues
+Host grenoble.iot-lab.info saclay.iot-lab.info
+ControlMaster auto
+ControlPersist yes
+ControlPath /tmp/ssh_mux_%h_%p_%r
 EOT
