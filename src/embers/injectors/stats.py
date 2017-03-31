974e878cc1d7f8cc89562203ff2d92a90b3b75ec
Olivier Fambon <olivier.fambon@inria.fr>
2017-03-31 08:17:24 +0200

    stats: trace exact nb of sent messages

--- src/embers/injectors/stats.py
+++ src/embers/injectors/stats.py
@@ -2,14 +2,14 @@ class stats:
     start_time = 0
     end_time = 0
     nb_devices = 0
-    nb_loops = 0
+    nb_sent = 0
     time_overflow = []
 
     def dump(self): dump_stats(self)
 
 
 def dump_stats(stats):
-    nb_sent = stats.nb_loops * stats.nb_devices
+    nb_sent = stats.nb_sent
     duration = stats.end_time - stats.start_time
     print("total: {} event{s} sent in {:.1f} sec.".format(
           nb_sent, duration, s="s" if nb_sent != 1 else ""))
