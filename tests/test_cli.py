898a34a038b4598bf75c6b3f1292d5e49f06cadd
Olivier Fambon <olivier.fambon@inria.fr>
2017-04-10 17:31:50 +0200

    tests: add test_injectors_no_config_no_init

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -23,3 +23,11 @@ def test_injectors_run_protocol(protocol):
 
 def test_injectors_run_many_parking():
     run("injectors --run --nb-devices 50 --events parking")
+
+
+def test_injectors_no_config_no_init(tmpdir, monkeypatch):
+    monkeypatch.chdir(tmpdir)
+    with pytest.raises(Exception) as ex_info:
+        run("injectors --run")
+
+    assert "failed to load config.py" in ex_info.value.output
