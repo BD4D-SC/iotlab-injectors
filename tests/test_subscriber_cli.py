855d079e943ea59a32dd33174523bc8b4a8daf23
Olivier Fambon <olivier.fambon@inria.fr>
2017-04-11 13:33:18 +0200

    tests: add test_subscriber_exit_after

--- tests/test_subscriber_cli.py
+++ tests/test_subscriber_cli.py
@@ -1,5 +1,21 @@
 from runner import run
 
+from embers.injectors.parallel import parallel_run
+
 
 def test_subscriber_no_args():
     run("subscriber")
+
+
+def test_subscriber_exit_after():
+
+    def subscriber():
+        run("subscriber --traffic --exit-after 2")
+
+    def injector():
+        run("injectors --run --event traffic --duration 0.02")
+
+    run("registry --init")
+    parallel_run([subscriber, injector], lambda func, thread: func())
+
+
