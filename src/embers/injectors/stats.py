9ae588056d90c0cae1c6755bb003e95daa63fc58
Olivier Fambon <olivier.fambon@inria.fr>
2017-10-17 15:03:42 +0200

    stats: handle per-round average

--- src/embers/injectors/stats.py
+++ src/embers/injectors/stats.py
@@ -1,6 +1,6 @@
 class stats:
-    start_time = 0
-    end_time = 0
+    send_time = 0
+    nb_rounds = 0
     nb_devices = 0
     nb_sent = 0
     time_overflow = []
@@ -10,8 +10,8 @@ class stats:
 
 def dump_stats(stats):
     nb_sent = stats.nb_sent
-    duration = stats.end_time - stats.start_time if stats.end_time else 0
-    print("total: {} event{s} sent in {:.1f} sec.".format(
+    duration = stats.send_time/stats.nb_rounds if stats.nb_rounds else 0
+    print("total: {} event{s} sent in {:.1f} sec/round".format(
           nb_sent, duration, s="s" if nb_sent != 1 else ""))
 
     if not stats.time_overflow: return
