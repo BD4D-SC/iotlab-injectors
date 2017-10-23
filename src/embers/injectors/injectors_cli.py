1d7110114c4cef7f0726e6e31b6de20ebe441847
Olivier Fambon <olivier.fambon@inria.fr>
2017-10-24 00:26:04 +0200

    injectors: add file-based devices reuse support

--- src/embers/injectors/injectors_cli.py
+++ src/embers/injectors/injectors_cli.py
@@ -96,6 +96,33 @@ def lookup_register_devices(events, nb_devices, offset):
     return gateway, devices+extra
 
 
+def file_lookup_register_devices(events, nb_devices, offset):
+    devices = registry.load_devices(events)
+    targets = devices[offset:offset+nb_devices]
+    replace = check_working(targets, events)
+    if replace:
+        targets = replace
+        devices = devices[0:offset] + targets + devices[offset+nb_devices:]
+
+    nb_extra = offset+nb_devices - len(devices)
+    gateway, extra = register_devices(events, nb_devices=nb_extra)
+
+    if extra or replace:
+        registry.save_devices(events, devices+extra)
+
+    return gateway, targets+extra
+
+
+def check_working(targets, events):
+    working = registry.check_devices(targets)
+    if working == targets:
+        return []
+    nb_dead = len(targets) - len(working)
+    replace = registry.register_devices(events, nb_dead)
+    print("replaced {} device{s}".format(nb_dead, s=s(nb_dead)))
+    return working + replace
+
+
 def s(nb):
     return "s" if nb != 1 else ""
 
